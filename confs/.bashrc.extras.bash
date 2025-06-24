364723fb4419c9b293095aa546efac0b98264135
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-24 09:09:56 -0600

    rename base confs dir to common (*nix platform agnostic)

--- confs/.bashrc.extras.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# SUPPORT FOR FLATPAK
-export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$XDG_DATA_DIRS"
-export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
