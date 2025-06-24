26d3a0b3db9c49c4cb6f4d6aee954b66bc8a61ff
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-24 11:30:56 -0600

    put non-hidden user bin directory on the path

--- common/.bash_config/path.bash
+++ common/.bash_config/path.bash
@@ -3,6 +3,10 @@ if [ ! -d "$HOME/.local/bin" ]; then
 fi
 export PATH="$HOME/.local/bin:$PATH"
 
+if [ -d "$HOME/bin" ]; then 
+	export PATH="$HOME/bin:$PATH"
+fi
+
 if [ -f "$HOME/.cargo/env" ]; then 
 	source "$HOME/.cargo/env"
 fi
